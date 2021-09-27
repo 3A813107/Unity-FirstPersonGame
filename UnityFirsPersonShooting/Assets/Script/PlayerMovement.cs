b6fc856e021fd3607752bf704c6bc30c54bdd944
3A813107 <90887372+3A813107@users.noreply.github.com>
2021-09-27 16:41:37 +0800

    Update PlayerMovement

--- UnityFirsPersonShooting/Assets/Script/PlayerMovement.cs
+++ UnityFirsPersonShooting/Assets/Script/PlayerMovement.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-    public float Speed = 12f;
+    public float WalkSpeed = 12f;
+    public float RunSpeed=20f;
+    private float Speed;
     public float gravity = -9.81f;
     public Transform groundCheak;
     public float geoundDistance = 0.4f;
@@ -17,28 +19,57 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
+    void Start()
+    {
+       Speed = WalkSpeed;
+    }
     void Update()
     {
-        isGrounded =  Physics.CheckSphere(groundCheak.position,geoundDistance,groundMask);
+        GroundCheak();
+        Jump();
+        Gravity();
+        Run();
+        Move();
+    }
 
+    private void Gravity()
+    {
         if(isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
-        float x= Input.GetAxis("Horizontal");
-        float z= Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward*z;
-
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+    private void Jump()
+    {
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y=Mathf.Sqrt(jumpHight * -2f *gravity);
         }
-
-        controller.Move(move * Speed*Time.deltaTime);
-
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
     }
+    private void Move()
+    {
+        float x= Input.GetAxisRaw("Horizontal");
+        float z= Input.GetAxisRaw("Vertical");
+        Vector3 move = transform.right * x + transform.forward*z;
+        controller.Move(move * Speed*Time.deltaTime);
+    }
+    private void GroundCheak()
+    {
+        isGrounded =  Physics.CheckSphere(groundCheak.position,geoundDistance,groundMask);
+    }
+
+    private void Run()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Speed=RunSpeed;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            Speed= WalkSpeed;
+        }
+    }
+
 }
