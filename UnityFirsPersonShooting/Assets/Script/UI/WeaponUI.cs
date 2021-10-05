995cb999672677a440d5f9b1a03add8992383a3e
3A813107 <90887372+3A813107@users.noreply.github.com>
2021-10-05 16:07:20 +0800

    Update WeaponUI

--- UnityFirsPersonShooting/Assets/Script/UI/WeaponUI.cs
+++ UnityFirsPersonShooting/Assets/Script/UI/WeaponUI.cs
@@ -8,10 +8,12 @@ public class WeaponUI : MonoBehaviour
     [SerializeField]private Image icon;
     [SerializeField]private Text magazineSizeText;
     [SerializeField]private Text magazineCountText;
+    [SerializeField]private Text weaponNameText;
 
-    public void UpdateInfo(Sprite weaponIcon,int magazineSize,int magazineCount)
+    public void UpdateInfo(Sprite weaponIcon,int magazineSize,int magazineCount,string weaponName)
     {
         icon.sprite = weaponIcon;
+        weaponNameText.text=weaponName;
         magazineSizeText.text=magazineSize.ToString();
         int magazineCountAmount = magazineSize * magazineCount;
         magazineCountText.text=magazineCountAmount.ToString();
