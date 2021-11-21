0822cc1ef1ab9cfed38a86239f012854a0860c64
3A813107 <90887372+3A813107@users.noreply.github.com>
2021-11-21 19:50:58 +0800

    Update MainMenu

---
+++ UnityFirsPersonShooting/Assets/Script/UI/MainMenuSwitcher.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class MainMenuSwitcher : MonoBehaviour
+{
+    public CinemachineVirtualCamera currentCamera;
+    void Start()
+    {
+        currentCamera.Priority++;
+    }
+
+    public void UpdateCamera(CinemachineVirtualCamera target)
+    {
+        currentCamera.Priority--;
+        currentCamera = target;
+        currentCamera.Priority++;
+    }
+}
